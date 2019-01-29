70e4fecc587c2e6161e7091b59e05eb2c1e3786b
unknown <brett_matsumoto@yahoo.com>
2019-01-28 21:56:34 -1000

    Completed 1

--- js/app.js
+++ js/app.js
@@ -3,6 +3,22 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var navButtons = document.getElementsByClassName('navi');
+// console.log(navButtons)
+
+for(i=0;i<navButtons.length;i++){
+    navButtons[i].addEventListener('click', innerSwitch)
+}
+
+function innerSwitch(){
+    var switchIt = this.querySelectorAll('.inner')[0]
+    if(switchIt.style.display === 'block'){
+        switchIt.style.display = 'none';
+    } else {
+        switchIt.style.display = 'block';
+    }
+}
+
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
